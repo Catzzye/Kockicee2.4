686c722c3bbd044f4c79e008efea7aa6e8bb9258
Norman <catzzye@gmail.com>
2018-04-02 11:41:19 +0200

    Initial Commit

---
+++ Kockicee2.4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kockicee2.4
+//
+//  Created by Marijeta Podgorsek on 02/04/2018.
+//  Copyright © 2018 Norman Podgoršek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
