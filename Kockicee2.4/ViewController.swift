a5bff49f53336b045ddc7f012214304509e9e5fd
Norman <catzzye@gmail.com>
2018-04-02 19:01:49 +0200

    Dodani labeli za pracenje dobivenih brojeva. 100% funkcionalno.

--- Kockicee2.4/ViewController.swift
+++ Kockicee2.4/ViewController.swift
@@ -14,10 +14,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
     
     let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6", ]
-    
-    	
     
     var randomDiceIndex1 : Int = 0
     var randomDiceIndex2 : Int = 0
@@ -39,7 +39,28 @@ class ViewController: UIViewController {
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceIndex2 = Int(arc4random_uniform(6))
         
-        print(randomDiceIndex1)
+        switch(randomDiceIndex1){
+        case 0: label1.text = "1";
+        case 1: label1.text = "2";
+        case 2: label1.text = "3";
+        case 3: label1.text = "4";
+        case 4: label1.text = "5";
+        case 5: label1.text = "6";
+        default: label1.text = "GREŠKA!"
+        }
+        
+        switch(randomDiceIndex2){
+        case 0: label2.text = "1";
+        case 1: label2.text = "2";
+        case 2: label2.text = "3";
+        case 3: label2.text = "4";
+        case 4: label2.text = "5";
+        case 5: label2.text = "6";
+        default: label2.text = "GREŠKA!"
+        }
+        
+        print(randomDiceIndex1 + 1)
+        print(randomDiceIndex2 + 1)
         
         diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
         diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
@@ -60,14 +81,15 @@ class ViewController: UIViewController {
     
     override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
         if motion == .motionShake {
-            
             updateDiceImages()
-            
-            
         }
         
     }
     
+    
+    
+    
+
     
     
     
