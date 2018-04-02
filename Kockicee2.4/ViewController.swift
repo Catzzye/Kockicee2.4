52e45453d7fdbf9618a921a1e867cdcf63f8c260
Norman <catzzye@gmail.com>
2018-04-02 16:40:45 +0200

    Uspješno uspostavljen UI i osnovna funkcionalnost aplikacije. Dodatne funkcije: -trešnjom mobitela bacaju se kockice - 100% funkcionalno -zvuk pri bacanju - nije implementirano -label koji prikazuju u numeričkom obliku dobivene brojeve - nije implementirano -podrška za landscape položaj - nije implementirano

--- Kockicee2.4/ViewController.swift
+++ Kockicee2.4/ViewController.swift
@@ -9,17 +9,67 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6", ]
+    
+    	
+    
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
+    func updateDiceImages() {
+        
+        
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        print(randomDiceIndex1)
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+        
+    }
+    
+    
+    
+    
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    
+    
+    
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            
+            updateDiceImages()
+            
+            
+        }
+        
+    }
+    
+    
+    
+    
 }
 
